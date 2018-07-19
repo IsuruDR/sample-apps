7cd3b72518eabc635ff95e4cd9d4725232a2fa01
pdkgamage <pdkgamage@gmail.com>
2018-07-19 12:07:34 +0800

    changed sms dn response

--- src/main/java/com/wso2telco/dep/sampleapp/model/SMSDNResponse.java
+++ src/main/java/com/wso2telco/dep/sampleapp/model/SMSDNResponse.java
@@ -3,20 +3,23 @@ package com.wso2telco.dep.sampleapp.model;
 
 public class SMSDNResponse {
 
-   private DeliveryInfoNotfication deliveryInfoNotfication;
+   private DeliveryInfoNotification deliveryInfoNotification;
 
-    public DeliveryInfoNotfication getDeliveryInfoNotfication () {
-        return deliveryInfoNotfication;
+    public DeliveryInfoNotification getDeliveryInfoNotification() {
+        return deliveryInfoNotification;
     }
 
-    public void setDeliveryInfoNotfication (DeliveryInfoNotfication deliveryInfoNotfication){
-        this.deliveryInfoNotfication = deliveryInfoNotfication;
+    public void setDeliveryInfoNotification(DeliveryInfoNotification deliveryInfoNotification) {
+        this.deliveryInfoNotification = deliveryInfoNotification;
     }
 
-    public class DeliveryInfoNotfication {
+    public class DeliveryInfoNotification {
 
         private String callbackData;
-        private deliveryInfo deliveryInfo;
+        private String serverReferenceCode;
+        private String messageReferenceCode;
+        private String senderAddress;
+        private DeliveryInfo deliveryInfo;
 
 
         public String getCallbackData () {
@@ -27,17 +30,59 @@ public class SMSDNResponse {
             this.callbackData = callbackData;
         }
 
-        public deliveryInfo getDeliveryInfo () {
+        public String getServerReferenceCode() {
+            return serverReferenceCode;
+        }
+
+        public void setServerReferenceCode(String serverReferenceCode) {
+            this.serverReferenceCode = serverReferenceCode;
+        }
+
+        public String getMessageReferenceCode() {
+            return messageReferenceCode;
+        }
+
+        public void setMessageReferenceCode(String messageReferenceCode) {
+            this.messageReferenceCode = messageReferenceCode;
+        }
+
+        public String getSenderAddress() {
+            return senderAddress;
+        }
+
+        public void setSenderAddress(String senderAddress) {
+            this.senderAddress = senderAddress;
+        }
+
+        public DeliveryInfo getDeliveryInfo () {
             return deliveryInfo;
         }
 
-        public void setDeliveryInfo (deliveryInfo deliveryInfo) {
+        public void setDeliveryInfo (DeliveryInfo deliveryInfo) {
             this.deliveryInfo = deliveryInfo;
         }
 
-        public class deliveryInfo {
+        public class DeliveryInfo {
+            private String operatorCode;
+            private String filterCriteria;
             private String address;
             private String deliveryStatus;
+
+            public String getOperatorCode() {
+                return operatorCode;
+            }
+
+            public void setOperatorCode(String operatorCode) {
+                this.operatorCode = operatorCode;
+            }
+
+            public String getFilterCriteria() {
+                return filterCriteria;
+            }
+
+            public void setFilterCriteria(String filterCriteria) {
+                this.filterCriteria = filterCriteria;
+            }
 
             public String getAddress() {
                 return address;
@@ -60,7 +105,27 @@ public class SMSDNResponse {
     }
 
 
+
 }
+
+
+/*{
+        "deliveryInfoNotification":{
+        "callbackData":"test call",
+        "serverReferenceCode":"OB-3003-8496abbb39e54275ae0f602bccebcf4c",
+        "messageReferenceCode":"8496abbb39e54275ae0f602bccebcf4c-0",
+        "senderAddress":"tel:+87711",
+        "deliveryInfo":{
+        "operatorCode":"DIALOG",
+        "filterCriteria":"test9",
+        "address":"tel:+94773851112",
+        "deliveryStatus":"DeliveredToNetwork"
+        }
+        }
+        }*/
+
+
+
 
     /*
 
